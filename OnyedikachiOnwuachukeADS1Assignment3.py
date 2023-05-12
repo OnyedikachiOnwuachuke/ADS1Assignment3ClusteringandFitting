a505a11c6f118543e45c56f6ae8f54e418b3df33
OnyedikachiOnwuachuke <125889723+OnyedikachiOnwuachuke@users.noreply.github.com>
2023-05-12 09:59:29 +0100

    Add files via upload

--- OnyedikachiOnwuachukeADS1Assignment3.py
+++ OnyedikachiOnwuachukeADS1Assignment3.py
@@ -46,18 +46,47 @@ file_name = 'Agricandforest%GDPworldbankdata.csv'
 agric_forest = read_and_clean_data(file_name)
 print(agric_forest)
 
-# Selecting random countries from the Dataframe to for the anaalysis
-countries = ['Benin', 'Burkina Faso', 'Bangladesh', 'Brazil', 'Botswana', 'Chile',
-       'China', "Cote d'Ivoire", 'Congo, Rep.', 'Costa Rica', 'Ecuador',
-       'Egypt, Arab Rep.', 'France', 'Gabon', 'Ghana', 'Guyana', 'Honduras',
-       'India', 'Iran, Islamic Rep.', 'Kenya', 'Korea, Rep.', 'Sri Lanka',
-       'Lesotho', 'Malawi', 'Malaysia', 'Niger', 'Pakistan', 'Philippines',
-       'Sudan', 'Senegal', 'Singapore', 'Suriname', 'Eswatini', 'Chad', 'Togo',
-       'Thailand', 'Turkiye', 'Uganda', 'South Africa', 'Zambia']
-selected_countries = agric_forest.loc[(countries)]
-print(selected_countries)
 
-# Selecting the years at ten years intervals from 1970 t0 2020 for more comprehensive analysis
-agric_forest_countries = selected_countries[["1960", "1970", "1980", "1990", "2000", "2010", "2020"]]
-agric_forest_countries = agric_forest_countries.drop(["1960"], axis=1)
+"""
+Creating a def function to select random countries and specified years to be used
+for futher analysis
+"""
+def select_countries_and_years(df, countries, years):
+    """
+    Selects specified countries and years from the given DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to select data from.
+    countries (list of str): The list of countries to select.
+    years (list of str): The list of years to select.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing data for the specified countries and years.
+    """
+    # Selecting random countries from the DataFrame for the analysis
+    selected_countries = df.loc[countries]
+
+    # Selecting the years at ten years intervals from 1970 to 2020 for more comprehensive analysis
+    selected_data = selected_countries[years]
+
+    return selected_data
+
+# Define the list of countries
+countries = ['Benin', 'Burkina Faso', 'Bangladesh', 'Brazil', 'Botswana', 'Chile',
+   'China', "Cote d'Ivoire", 'Congo, Rep.', 'Costa Rica', 'Ecuador',
+   'Egypt, Arab Rep.', 'France', 'Gabon', 'Ghana', 'Guyana', 'Honduras',
+   'India', 'Iran, Islamic Rep.', 'Kenya', 'Korea, Rep.', 'Sri Lanka',
+   'Lesotho', 'Malawi', 'Malaysia', 'Niger', 'Pakistan', 'Philippines',
+   'Sudan', 'Senegal', 'Singapore', 'Suriname', 'Eswatini', 'Chad', 'Togo',
+   'Thailand', 'Turkiye', 'Uganda', 'South Africa', 'Zambia']
+
+# Define the years of interest
+years = ["1970", "1980", "1990", "2000", "2010", "2020"]
+
+# Use the function
+agric_forest_countries = select_countries_and_years(agric_forest, countries, years)
 print(agric_forest_countries)
+
+#Plotting a scatter matrix on the dataframe to show the least correlated years for further analysis
+pd.plotting.scatter_matrix(agric_forest_countries, figsize=(9, 9), s=5, alpha=0.8)
+plt.show()
