1506b5ec7cfae5863ed6efb7708c12375e1a1bd0
OnyedikachiOnwuachuke <125889723+OnyedikachiOnwuachuke@users.noreply.github.com>
2023-05-12 09:10:34 +0100

    Add files via upload

---
+++ OnyedikachiOnwuachukeADS1Assignment3.py
@@ -0,0 +1,14 @@
+# Importing the necessary Libraries needed for this Assignment
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import sklearn.cluster as cluster
+import sklearn.metrics as skmet
+import matplotlib.pyplot as plt
+import cluster_tools as ct
+import scipy.optimize as opt
+from scipy.optimize import curve_fit
+import errors as err
+
+
+
